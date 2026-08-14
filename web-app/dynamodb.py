29ce3cabfa253f0f4b50611de4ffa59f16b5d702
Haik_Gr <haykgrigoryanpersonal@gmail.com>
2026-08-14 11:31:37 +0400

    added explicitly region

--- web-app/dynamodb.py
+++ web-app/dynamodb.py
@@ -7,7 +7,7 @@ TABLE_NAME = os.getenv(
     "distributed-processing"
 )
 
-dynamodb = boto3.resource("dynamodb")
+dynamodb = boto3.resource("dynamodb", region_name="us-east-1")
 
 table = dynamodb.Table(TABLE_NAME)
 
